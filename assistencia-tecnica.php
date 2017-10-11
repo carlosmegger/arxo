613ea4f4f5ad651ab9540ed1a2ebac68847a12ae
Carlos Megger <carlosmegger@gmail.com>
2017-10-11 10:19:50 -0300

    ajustes arxo home e etc

---
+++ assistencia-tecnica.php
@@ -0,0 +1,13 @@
+<? include('topo.php'); ?>
+	<div class="breadcrumb">
+		<div class="central">
+			<a href="./">HOME</a> &raquo; <a href="">ASSISTÊNCIA TÉCNICA</a> 
+		</div>
+	</div>
+	<section class="conteudo">
+		<div class="central">
+			<h1>ASSISTÊNCIA TÉCNICA</h1>
+			<p>ASSISTÊNCIA TÉCNICA</p>
+		</div>
+	</section>
+<? include('rodape.php') ?>
